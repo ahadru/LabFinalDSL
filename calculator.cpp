e8254fb10f9e6b1d998dc67e6d6c3bd6211627cb
Ahad <ahadsheikh1999@gmail.com>
2018-05-10 09:48:47 +0600

    Main file

---
+++ calculator.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    double a,b;
+    cin>>a>>b;
+    cout<<"Sum result\n";
+    cout<<sum(a,b)<<endl;
+
+    cin>>a>>b;
+    cout<<"Sub result\n";
+    cout<<sub(a,b)<<endl;
+
+    cin>>a>>b;
+    cout<<"Mul result\n"
+    cout<<mul(a,b)<<endl;
+    
+    cin>>a>>b;
+    cout<<"Mod result\n"
+    cout<<mod(a,b)<<endl;
+
+    cin>>a>>b;
+    cout<<"Div result\n"
+    cout<<div(a,b)<<endl;
+    return 0;
+}
